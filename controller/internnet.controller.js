da1b4abc731b9a7fe00c4847ad059cc2d8ea2da9
Allen Jamison <allen@gmail.com>
2024-08-07 21:36:15 +0800

    Added post announcement query

--- controller/internnet.controller.js
+++ controller/internnet.controller.js
@@ -1,5 +1,6 @@
 const postgre = require('../database')
 const internnetController = {
+    // get all users
     getAll: async(req, res) => {
         try {
             const { rows } = await postgre.query("SELECT * FROM users")
@@ -9,9 +10,25 @@ const internnetController = {
             res.json({msg: error.msg})
         }
     },
+    
+    // get one by username
     getOne: async(req, res) => {
         try {
             const { rows } = await postgre.query(`SELECT * FROM users WHERE username = '${req.params.username}'`)
+            console.log(res)
+            res.json({msg: "OK", data: rows})
+        } catch (error) {
+            res.json({msg: error.msg})
+        }
+    },
+
+    // post announcement
+    postAnnouncement: async(req, res) => {
+        const { title, description, id } = req.body
+
+        try {
+            const { rows } = await postgre.query(`INSERT INTO announcements (title, description, date_posted, time_posted, user_id) VALUES
+                ('${title}', '${description}', CURRENT_DATE, CURRENT_TIME, '${id}') RETURNING *`)
             console.log(res)
             res.json({msg: "OK", data: rows})
         } catch (error) {
