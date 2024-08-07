26b03e149e93c3f9ac608a091cfa17b1f47b497a
Allen Jamison <119152205+Teqnixx@users.noreply.github.com>
2024-08-07 22:30:01 +0800

    Update internnet.controller.js

--- controller/internnet.controller.js
+++ controller/internnet.controller.js
@@ -30,7 +30,10 @@ const internnetController = {
             const sql = 'INSERT INTO announcements(title, description, date_posted, time_posted, user_id) VALUES($1, $2, current_date, current_time, $3) RETURNING *'
 
             const { rows } = await postgre.query(sql, [ title, description, id ])
-            
+
+            if(error) {
+                throw error
+            }
             console.log(res)
             res.json({msg: "OK", data: rows})
         } catch (error) {
