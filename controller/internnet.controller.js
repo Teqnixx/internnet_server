4ce9838c23dc59e25158231f738c6d1ba18d2db0
Allen Jamison <allen@gmail.com>
2024-08-07 14:52:15 +0800

    Added Get One Query

--- controller/internnet.controller.js
+++ controller/internnet.controller.js
@@ -8,6 +8,15 @@ const internnetController = {
         } catch (error) {
             res.json({msg: error.msg})
         }
+    },
+    getOne: async(req, res) => {
+        try {
+            const { rows } = await postgre.query(`SELECT * FROM users WHERE email = ${req.params.email}`)
+            console.log(res)
+            res.json({msg: "OK", data: rows})
+        } catch (error) {
+            res.json({msg: error.msg})
+        }
     }
 }
 
--- routes/internnet.router.js
+++ routes/internnet.router.js
@@ -4,5 +4,6 @@ const router = express.Router()
 const internnetController = require('../controller/internnet.controller.js')
 
 router.get("/users", internnetController.getAll)
+router.get("/users/:email", internnetController.getOne)
 
 module.exports = router
